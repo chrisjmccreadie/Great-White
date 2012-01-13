5dee4ff143fadad4172cbd6d93cf11cdbd30887c
John-Michael Glenn <phyre19@gmail.com>
2012-01-13 13:49:38 -0800

    Change config to work with PHPFog setup scripts

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'db_host';
+$db['default']['username'] = 'db_user';
+$db['default']['password'] = 'db_pass';
+$db['default']['database'] = 'db_name';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
